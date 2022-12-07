9a52a15ab20ba69121e9e68dba7a7c921de62f50
Paola Guerra <paola@192.168.1.9>
2022-12-06 22:56:08 -0300

    definir modelo country

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -3,10 +3,49 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
+  
+// x  ID (Código de 3 letras) *
+// x Nombre *
+// x Imagen de la bandera *
+// x Continente *
+// x Capital *
+// x Subregión
+// x Área
+// Población
+
   sequelize.define('country', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    id:{
+      type:DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true
+    },
+    flag:{
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    continent:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion:{
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    area:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    population:{
+      type: DataTypes.STRING,
+      allowNull: true
     },
   });
 };
